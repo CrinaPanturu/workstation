d11e0dc771c70ad0cffd84a168c9b5c269372297
vagrant <vagrant@localhost.localdomain>
2021-07-19 13:20:09 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,16 @@
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/file/motd' do
+  content 'This server is the property of Crina'
+  action :create
+  owner 'root'
+  group 'root'
+end
