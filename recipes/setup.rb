e5ee8ccb5f7b83cd0c33ba59294c87bda91b76c1
crina.panturu <crina.ctp@gmail.com>
2021-07-21 11:17:01 +0000

    New details added in setup recipe

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,8 +10,13 @@ end
 
 package 'ntp'
 
-file '/file/motd' do
-  content 'This server is the property of Crina'
+file '/etc/motd' do
+  content 'This server is the property of Crina
+  HOSTNAME: localhost.localdomain
+  IPADDRESS: 10.0.2.15
+  CPU: 1896.003
+  MEMORY: 1016860
+'
   action :create
   owner 'root'
   group 'root'
